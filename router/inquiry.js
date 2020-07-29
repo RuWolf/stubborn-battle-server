416a4d4951232e7f8ed36a2300d1b67f339d776d
Rudolf <bechstedt@nanosemantics.ai>
2020-07-29 23:06:32 +0300

    test heroku

--- router/inquiry.js
+++ router/inquiry.js
@@ -3,7 +3,7 @@ const router = Router();
 const fetch = require('node-fetch');
 
 router.post('/init', async (req, res) => {
-  const uuid = req.body;
+  /* const uuid = req.body;
   const response = await fetch("https://biz.nanosemantics.ru/api/bat/nkd/json/Chat.init", {
     headers: {
       "Content-Type": "application/json"
@@ -14,8 +14,8 @@ router.post('/init', async (req, res) => {
     })
   });
   let result = await response.json();
-  console.log(result.result)
-  res.json({result: result.result})
+  console.log(result.result) */
+  res.json({result: 'result.result'})
 });
 
 router.post('/request', async (req, res) => {
